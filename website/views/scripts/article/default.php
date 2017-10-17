0f77c3acc6e2b14a44e1f00d5a87d5a13e3cd444
ferdi <ferdireza96@gmail.com>
2017-10-17 15:47:41 +0700

    fix no spacing between col mobile view

--- website/views/scripts/article/default.php
+++ website/views/scripts/article/default.php
@@ -10,11 +10,14 @@
         <div class="card">
           <img class="card-img-top" src="<?php echo $data->getContentimage(); ?>" style="height: 150px;" alt="<?php echo $data->getTitle(); ?>">
           <div class="card-body">
-            <h4 class="card-title"><?php echo $data->getTitle(); ?></h4>
+            <h5 class="card-title"><?php echo $data->getTitle(); ?></h5>
             <span class="card-subtitle mb-2 text-primary"><?php echo $data->getAuthor().", ".$data->getCreated(); ?></span>
             <p class="card-text text-muted"><?php echo substr($data->getContent(),0,200)."....."; ?></p>
             <a href="<?php echo Pimcore\Tool::getHostUrl()."/articles/".$linker; ?>" class="btn btn-info"><i class="fa fa-eye"></i> Read More</a>
           </div>
+        </div>
+        <div class="pb-3">
+
         </div>
       </div>
     <?php } ?>
